62c09806a92be3d9d95897f1f698d86767e43b7a
chloewigsten <chloewigsten@gmail.com>
2022-09-13 12:05:51 -0400

    add all information to server

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
 //import dependencies 
+const express = require('express');
+const cors = require('cors');
+
+//import json files 
+const projects = require('./projects.json')
+const about = require('./about.json')
+
+//create app object
+const app = express();
+
+//middleware 
+app.use(cors());
+
+//home route for testing app
+app.get('/', (req,res) => {
+    res.send('Hello world');
+})
+
+//route for retrieving projects
+app.get('/projects', (req,res) => {
+    res.json(projects)
+})
+
+//route for retrieving about 
+app.get('/about', (req,res) => {
+    res.json(about)
+})
+
+//declare PORT
+const PORT = process.env.PORT || 5000;
+
+//Listen for PORT 
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
